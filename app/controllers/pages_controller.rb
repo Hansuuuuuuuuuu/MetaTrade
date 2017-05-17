fd125b5dbeb68b0d5eae064ef09e924695cf8923
Hans Cristobal <hanschristiancristobal@gmail.com>
2017-05-17 22:18:39 +0800

    downcase matching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,10 +13,11 @@ class PagesController < ApplicationController
 
 	def card
 		@carddd =  MTG::Card.where(name: params[:cn]).all
+
 		@carddd = @carddd.map{|f| [f.name, f.image_url, f.text]}
 		@carddd = @carddd.first
 
-		@listings = UserListing.where(:card_name => @carddd[0])
+		@listings = UserListing.where('lower(card_name) = ?', @carddd[0].downcase)
 	end
 
 	def traders
